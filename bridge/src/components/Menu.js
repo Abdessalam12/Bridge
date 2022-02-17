77ff909489a73062c4d51e3689e962cd8749c300
Abdessalam <abdessalambahri8@gmail.com>
2022-02-17 23:19:37 +0100

    true descovry

--- bridge/src/components/Menu.js
+++ bridge/src/components/Menu.js
@@ -15,19 +15,19 @@ render(){
     return (
     
         <>   
-        <Navbar  bg ="light" variant="dark">
-           Bridge
-        </Navbar>
-        <Container>
-        <Navbar  className='lmj-banner' variant="light">
-        
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-       
-      </Navbar>
-        </Container>
+        <Nav variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">Active</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+      Disabled
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
         
      
       </>
